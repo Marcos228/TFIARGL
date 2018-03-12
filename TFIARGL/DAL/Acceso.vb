1c96d534138f93899bea23f8a002de26d3e6fc41
Marcos <marcos.tassara@gmail.com>
2018-03-12 17:09:07 -0300

    Inicio

---
+++ TFIARGL/DAL/Acceso.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+Public Class Acceso
+    Public Function Lectura(command As SqlCommand) As DataTable
+
+    End Function
+
+    Public Function Scalar(command As SqlCommand) As DataTable
+
+    End Function
+    Public Function TraerID(IDGenerico As String, TablaGenerica As String) As Integer
+
+    End Function
+
+    Public Function Escritura(command As SqlCommand) As Integer
+
+    End Function
+
+    Public Function MiComando(consulta As String) As SqlCommand
+
+    End Function
+    Public Function MiconexionMaster() As SqlConnection
+
+    End Function
+
+    Public Function Miconexion() As SqlConnection
+
+    End Function
+End Class
